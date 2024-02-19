e17bf5a1d46fb9b2b6a739b12eaa98637426cc61
ahrorhalimboyev <ahrorhalimboyev9302@gmail.com>
2024-02-19 23:15:33 +0500

    first commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+  async onModuleInit() {
+    return this.$connect();
+  }
+
+  async onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
